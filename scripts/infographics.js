7f7d4a8313f1a4a0cd6be729db3bb9c74f2458fb
Akira Maeda <64563041+AKMaaa@users.noreply.github.com>
2022-02-08 16:19:50 +0900

    Add files via upload

--- scripts/infographics.js
+++ scripts/infographics.js
@@ -1,10 +1,21 @@
 let img_yama;
 let img_saka;
 let img_bay;
+let img_train1;
+let img_train2;
+let img_yazirusi;
+let img_tw1;
+let img_tw2;
+
 function preload(){
   img_yama=loadImage("imagesp5/yama.png");
   img_saka=loadImage("imagesp5/saka.png");
   img_bay=loadImage("imagesp5/bay.png");
+  img_train1=loadImage("imagesp5/train1.png");
+  img_train2=loadImage("imagesp5/train2.png");
+  img_yazirusi=loadImage("imagesp5/yazirusi.png");
+  img_tw1=loadImage("imagesp5/tower1.png");
+  img_tw2=loadImage("imagesp5/tower2.png");
 }
 
 function setup() {
@@ -18,6 +29,8 @@ function draw() {
   yama();
   saka();
   bay();
+  train();
+  tower();
 }
 
 var yx=0,yy=0;
@@ -143,7 +156,7 @@ textSize(20);
 text("#02. Hatiman-zaka",700,385);
 }
 
-/*金森倉庫*/ 
+/*金森倉庫*/
 var kf=1;
 var kx=0;
 var bkx=0;
@@ -191,6 +204,7 @@ function bay(){
     push();
     translate(i-5,290);
     rotate(alg);
+    fill(30,10,100);
      rect(5,-40,20,80);
     pop();
   }
@@ -237,6 +251,280 @@ textSize(20);
 text("#03. Kanemori Red Brick Warehouse",1200,385);//x=700 つまり+500でおけ
 }
 
+var tf=1;
+var tx=0;
+var ty=0;
+var stx=0;
+var sty=0;
+var stf=0;//ストッパーフラグ変数
+var fload=0;//マウスの距離を図る用の変数
+var load=0;
+var balltx=0;
+var plusty=0;
+
+function train(){
+  plusty+=0.08;
+  image(img_yazirusi,20-abs(sin(plusty))*10,650+abs(sin(plusty))*10,img_yazirusi.width*0.1,img_yazirusi.height*0.1);
+  image(img_train1,100,550,img_train1.width*1.1,img_train1.height*1.1);/*450加算*/
+  fill(0,0,98);
+  strokeWeight(0.4);
+  if(mouseX>120&&mouseX<170&&mouseY>850&&mouseY<900){/*条件分岐 */
+   fill(0,0,92);
+   if(mouseIsPressed==true&&tf==0){
+     mouseIsPressed=false;
+   tf=1;
+   }else if(mouseIsPressed==true&&tf==1){
+     mouseIsPressed=false;
+   tf=0;
+   }
+  }
+  rect(120,850,50,50,30);
+  if(tf==1){
+   fill(207,50,90);
+   rect(130,860,30,30,30);//ボタン部分
+  }
+
+push();
+translate(150,600);
+fill(50,10,100);
+rotate(PI/11);
+stroke(0);
+rect(tx,ty,80,40);//電車部分
+pop();
+if(tf==1){
+if(mouseX>180&&mouseX<250&&mouseY>640&&mouseY<660){//ストッパー　ホバー用の変数
+　　fill(0,90,90);//ホバー時一時色を変更する
+  if(mouseIsPressed==true){
+    stf=1;
+  }
+}else {
+  if(mouseIsPressed==true){
+      stf=0;
+  }
+}
+}
+//stf が1の時にクリックしたと判定して以下の処理を行う。
+
+if(stf==1){
+  stx=mouseX-180;
+  sty=mouseY-640;
+}else if(stf==0){//サイドクリックで0に戻る
+  stx=0;
+  sty=0;
+}
+push();
+translate(180,640);//ストッパー部分180,640
+fill(50,20,100);
+stroke(0);
+rect(stx,sty,70,20);
+pop();
+  
+  strokeWeight(0.8);
+  stroke(300,10,10);
+  fill(360,50,90);
+  ellipse(600+balltx, 750, 50,50);//ビー玉*/
+
+  if(tf==1){
+    if(stx<-30){
+    tx+=4;
+    ty=3;
+    if(tx>370){
+       tx=370;
+       balltx++;
+       if(balltx>100){
+          stf=0;
+          balltx=0;
+       }
+    }
+    }
+    if(stf==0){
+        tx=0;
+        ty=0;
+        balltx=0;
+    }
+  }
+
+ if(tf==0){
+   tx=0;
+   ty=0;
+ }
+
+/*******************/
+  stroke(10,20);
+  fill(10,70);
+  textSize(20);
+  text("#04. Train",190,885);
+  image(img_train2,65,500,img_train2.width*0.4,img_train2.height*0.4);
+}
+
+var tof=1;
+var gb1x=0;
+var gb2x=0;
+var gb3x=0;
+var db1f=0;
+var db1r=0;
+var db2f=0;
+var db2r=0;
+var db3r=0;
+var db3f=0;
+var gb3y=0;
+var gf1=0;
+var gf2=0;
+var gf3=0;
+var gf4=0;
+var gf5=0;
+var gf6=0;
+function tower(){
+   image(img_tw2,500,500,img_tw2.width*0.8,img_tw2.height*0.8);
+   fill(0,0,98);
+   strokeWeight(0.4);
+   if(mouseX>1130&&mouseX<1180&&mouseY>650&&mouseY<700){/*条件分岐 */
+    fill(0,0,92);
+    if(mouseIsPressed==true&&tof==0){
+      mouseIsPressed=false;
+    tof=1;
+    }else if(mouseIsPressed==true&&tof==1){
+      mouseIsPressed=false;
+    tof=0;
+    }
+   }
+   rect(1130,650,50,50,30);//ボタン
+   if(tof==1){
+    fill(207,50,90);
+    rect(1140,660,30,30,30);
+   }
+
+   strokeWeight(0.8);
+   stroke(300,10,10);
+   fill(360,50,90);
+   ellipse(1800+gb1x, 1250, 50,50);//ビー玉1*/
+   ellipse(1165+gb2x, 960, 50,50);//ビー玉2*/
+   ellipse(1552+gb3x, 760+gb3y, 50,50);//ビー玉2*/
+
+   if(tof==1){
+     gb1x-=5;
+     if(gb1x<-630){
+          db1f=1;//段ボール到達したらの判定 1は到達判定の数値
+          if(gb1x<-670){
+                     gb1x=-670;
+          }
+     }
+   }
+
+   if(db1f==1){
+     db1r+=PI/180;
+     gb2x+=5;
+     if(gb2x>375){
+       db2f=1;
+       if(gb2x>405){
+             gb2x=405;
+       }
+     }
+     if(db1r>PI/22){
+           db1r=PI/22;
+     }
+   }
+   if(db2f==1){
+       db2r-=PI/180;
+       if(gf1==0){
+       gb3x-=5;
+       gb3y+=0.3;
+       }
+       if(gb3x<-540&&gb3x>-560){
+         if(gb3y<140){
+        gb3y+=4;
+         }
+        gf1=1;
+       }
+       if(db2r<-PI/22){
+              db2r=-PI/22;
+       }
+   }
+  
+if(gb3y>160){
+    gf2=1;
+}
+
+   if(gf1==1){//タワー入ったら
+      gb3y+=1;
+   }
+   if(gf2==1){
+      gb3y+=0.6;
+      gb3x-=2.6;
+      if(gb3y>230){
+        gf3=1;
+      }
+   }
+   if(gf3==1){
+         gb3x+=2.8;
+        gb3y+=1;
+        if(gb3y>280){
+                gf4=1;
+        }
+   }
+   if(gf4==1){
+            gb3x+=3.2;
+            gb3y+=0.1;
+            if(gb3y>370){
+                   gf5=1;
+            }
+   }
+
+   if(gf5==1){
+          gb3x-=6.0;
+          gb3y+=0.1;
+          if(gb3y>470){
+            gf6=1;
+          }
+   }
+   if(gf6==1){
+        gb3x+=4.0;
+        gb3y+=0.1;
+        if(gb3y>620){
+            tof=0;
+        }
+   }
+   strokeWeight(3);
+   stroke(0,50);
+
+   push();//第一段ボール
+   translate(1140,986);
+   rotate(db1r);
+   line(0,-100,0,280);
+   pop();
+
+   push();//第二段ボール
+   translate(1580,840);
+   rotate(db2r);
+   line(0,-150,0,280);
+   pop();
+   strokeWeight(1);
+
+  if(tof==0){
+    gb1x=0;
+    db1r=0;
+    db1f=0;
+    gb2x=0;
+    db2r=0;
+    db2f=0;
+    gb3x=0;
+    gb3y=0;
+    gf1=0;
+    gf2=0;
+    gf3=0;
+    gf4=0;
+    gf5=0;
+    gf6=0;
+  }
+/*******************/
+   stroke(10,20);
+   fill(10,70);
+   textSize(20);
+   text("#05. Goryokaku Tower",1200,685);
+   /*表の画像*/
+   image(img_tw1,500,500,img_tw1.width*0.8,img_tw1.height*0.8);
+}
+
 function grid(){
   strokeWeight(0.05);
   for( y=0;y<height;y+=100){
@@ -246,3 +534,4 @@ function grid(){
     }
   }
 }
+
